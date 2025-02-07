d72b853a4f0510317558329e9a8a861ea3cb3601
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-07 11:40:08 +0100

    feat(email-service): complete go rewrite

---
+++ service.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Would-You-Bot/email-microservice/tasks"
+	"github.com/jasonlvhit/gocron"
+	"github.com/joho/godotenv"
+	"github.com/Would-You-Bot/email-microservice/db"
+)
+
+func main() {
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	if err := db.Init(); err != nil {
+		log.Fatalf("Error initializing database: %v", err)
+	}
+
+	gocron.Every(1).Minute().Do(func() {
+		tasks.DeleteUnconfirmedUsers(db.Conn)
+	})
+
+	<-gocron.Start()
+}
