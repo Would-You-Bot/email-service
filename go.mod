509bff9035c4f7a7352d86a34b426d13da9d966a
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-08 16:52:27 +0100

    fix: add cron logging

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.23
 require (
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/jasonlvhit/gocron v0.0.1
-	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- service.go
+++ service.go
@@ -1,24 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/Would-You-Bot/email-microservice/db"
 	"github.com/Would-You-Bot/email-microservice/tasks"
 	"github.com/jasonlvhit/gocron"
-	"github.com/joho/godotenv"
-	"github.com/Would-You-Bot/email-microservice/db"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-    log.Fatal("Error loading .env file")
-  }
-
 	if err := db.Init(); err != nil {
 		log.Fatalf("Error initializing database: %v", err)
 	}
 
 	gocron.Every(1).Minute().Do(func() {
+		fmt.Printf("Running job - DeleteUnconfirmedUsers\n")
 		tasks.DeleteUnconfirmedUsers(db.Conn)
 	})
 
